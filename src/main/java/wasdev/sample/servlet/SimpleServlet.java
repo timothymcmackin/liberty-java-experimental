f8e3ba76fa20b7ce269a125a7425770e05a49900
Timothy McMackin <tmcmack@us.ibm.com>
2016-07-11 16:54:10 +0000

    Small update to see if running app changes

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().print("Tim greets you!");
+        response.getWriter().print("Tim greets you! Really! Don't you feel important?");
     }
 
 }
